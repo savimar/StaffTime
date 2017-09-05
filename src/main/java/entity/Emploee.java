2a409a20349fe4c4a365e8ee3f722f19cb57eb29
savimar <sam409@yandex.ru>
2017-09-05 17:41:24 +0300

    create entity

---
+++ src/main/java/entity/Emploee.java
@@ -0,0 +1,50 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "emploee", schema = "public", catalog = "stafftime")
+public class Emploee {
+    private long id;
+    private String name;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 2044)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Emploee emploee = (Emploee) o;
+
+        if (id != emploee.id) return false;
+        if (name != null ? !name.equals(emploee.name) : emploee.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/entity/Status.java
@@ -0,0 +1,52 @@
+package entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "status", schema = "public", catalog = "stafftime")
+public class Status{
+    private long id;
+    private LocalDateTime time;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "time", nullable = false)
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Status status = (Status) o;
+
+        if (id != status.id) return false;
+        if (time != null ? !time.equals(status.time) : status.time != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (time != null ? time.hashCode() : 0);
+        return result;
+    }
+}
