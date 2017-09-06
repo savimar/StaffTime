8725d6e77f4efab5253c0925f900561577748e54
savimar <sam409@yandex.ru>
2017-09-06 18:38:49 +0300

    add context

---
+++ src/main/java/ru/savimar/stafftime/config/Listener.java
@@ -0,0 +1,9 @@
+package ru.savimar.stafftime.config;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Listener extends ContextLoaderListener {
+}
