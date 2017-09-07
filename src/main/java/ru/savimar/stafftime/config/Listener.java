53709b87a87afd9834562a28dbbeabe2d889ffa3
savimar <sam409@yandex.ru>
2017-09-07 13:49:00 +0300

    Revert "add context"
    This reverts commit 8725d6e

--- src/main/java/ru/savimar/stafftime/config/Listener.java
+++
@@ -1,9 +0,0 @@
-package ru.savimar.stafftime.config;
-
-import org.springframework.web.context.ContextLoaderListener;
-
-import javax.servlet.annotation.WebListener;
-
-@WebListener
-public class Listener extends ContextLoaderListener {
-}
